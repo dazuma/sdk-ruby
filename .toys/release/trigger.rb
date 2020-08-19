72e141d550540b390a8fe6b0f763ca732c4d703f
Daniel Azuma <dazuma@gmail.com>
2020-08-19 11:57:52 -0700

    chore: Update release process

--- .toys/release/trigger.rb
+++ .toys/release/trigger.rb
@@ -7,26 +7,28 @@ include :terminal
 include :fileutils
 include "release-tools"
 
+required_arg :gem_name
 required_arg :version
-flag :yes
-flag :git_remote, default: "origin"
+
+flag :git_remote, "--git-remote=VAL", default: "origin"
+flag :yes, "--yes", "-y"
 
 def run
   cd context_directory
 
   puts "Running prechecks...", :bold
   verify_git_clean
-  verify_library_version version
-  changelog_entry = verify_changelog_content version
+  verify_library_version gem_name, version
+  changelog_entry = verify_changelog_content gem_name, version
   verify_github_checks
 
   puts "Found changelog entry:", :bold
   puts changelog_entry
-  if !yes && !confirm("Release cloud_events #{version}? ", :bold, default: true)
+  if !yes && !confirm("Release #{gem_name} #{version}? ", :bold, default: true)
     error "Release aborted"
   end
 
-  tag = "v#{version}"
+  tag = "#{gem_name}/v#{version}"
   exec ["git", "tag", tag]
   exec ["git", "push", git_remote, tag]
   puts "SUCCESS: Pushed tag #{tag}", :green, :bold
