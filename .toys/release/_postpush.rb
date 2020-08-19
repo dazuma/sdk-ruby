5e28903d5f482426d2fbc29894e27ea7c884eb00
Daniel Azuma <dazuma@gmail.com>
2020-08-19 11:57:52 -0700

    chore: Update release process

---
+++ .toys/release/_postpush.rb
@@ -0,0 +1,88 @@
+# frozen_string_literal: true
+
+desc "Post-push job that runs on merge of a release PR"
+
+flag :sha, "--sha=VAL", default: ::ENV["GITHUB_SHA"]
+flag :git_remote, "--git-remote=VAL", default: "origin"
+flag :result, "--result=VAL", default: "unknown"
+
+include :exec, exit_on_nonzero_status: true
+include :fileutils
+include :terminal, styled: true
+include "release-tools"
+
+def run
+  cd context_directory
+  verify_repo_identity git_remote: git_remote
+
+  pr = find_release_prs repo_path, merge_sha: sha
+  if pr
+    pr_number = pr["number"]
+    gem_name = pr["head"]["ref"].sub("release/", "")
+    logger.info "This appears to be a merge of release PR #{pr_number} for #{gem_name}."
+    if result == "success"
+      trigger_release pr_number, gem_name, pr
+    else
+      report_trigger_aborted pr_number, gem_name, pr
+    end
+  else
+    logger.info "This was not a merge of a release PR."
+    update_open_release_prs
+  end
+
+  logger.info "Done."
+  error "CI failed, so failing post-push." unless result == "success"
+end
+
+def trigger_release pr_number, gem_name, pr
+  logger.info "CI passed for the merge."
+  version = current_library_version gem_name
+  changelog = verify_changelog_content gem_name, version
+  logger.info "Creating Github release for #{gem_name} #{version} ..."
+  push_github_release sha, gem_name, version, changelog
+  logger.info "Updating release PR to report trigger success ..."
+  update_release_pr repo_path, pr_number,
+                    label:   release_triggered_label,
+                    message: "Triggered release of #{gem_name} #{version}.",
+                    cur_pr: pr
+end
+
+def report_trigger_aborted pr_number, gem_name, pr
+  logger.info "CI failed for the merge."
+  logger.info "Updating the release PR to report the error ..."
+  update_release_pr repo_path, pr_number,
+                    label:   release_error_label,
+                    message: "CI failed on merge. Did not trigger #{gem_name} release.",
+                    cur_pr: pr
+  logger.info "Opening a new issue to report the failure ..."
+  body = <<~STR
+    Release of: #{gem_name} #{current_library_version gem_name}
+    Release PR: ##{pr_number}
+    Commit: https://github.com/#{repo_path}/commit/#{sha}
+  STR
+  response = capture ["gh", "issue", "create", "--repo", repo_path,
+                      "--title", "Release PR ##{pr_number} failed CI.",
+                      "--body", body]
+  issue_number = ::JSON.parse(response)["number"]
+  logger.info "Issue #{issue_number} opened."
+end
+
+def update_open_release_prs
+  commit_message = capture ["git", "log", "-1", "--pretty=%B"]
+  pr_message = <<~STR
+    WARNING: An additional commit was added while this release PR was open.
+    You may need to add to the changelog, or close this PR and prepare a new one.
+
+    Commit link: https://github.com/#{repo_path}/commit/#{sha}
+
+    Message:
+    #{commit_message}
+  STR
+  logger.info "Searching for open release PRs..."
+  prs = find_release_prs repo_path
+  prs.each do |pr|
+    pr_number = pr["number"]
+    logger.info "Updating PR #{pr_number} ..."
+    update_release_pr repo_path, pr_number, message: pr_message, cur_pr: pr
+  end
+end
--- .toys/release/precheck.rb
+++ .toys/release/precheck.rb
@@ -2,18 +2,23 @@
 
 desc "Run release prechecks for the cloud_events gem"
 
+include :fileutils
 include :terminal
 include "release-tools"
 
+required_arg :gem_name
 required_arg :version
 
-def run
-  ::Dir.chdir context_directory
+flag :git_remote, "--git-remote=VAL", default: "origin"
 
-  puts "Running prechecks for releasing version #{version}...", :bold
+def run
+  cd context_directory
+
+  puts "Running prechecks for releasing #{gem_name} #{version}...", :bold
   verify_git_clean
-  verify_library_version version
-  verify_changelog_content version
+  verify_repo_identity git_remote: git_remote
+  verify_library_version gem_name, version
+  verify_changelog_content gem_name, version
   verify_github_checks
 
   puts "SUCCESS", :green, :bold
